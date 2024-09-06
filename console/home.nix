74e1cd649742bad3144b4aa435fd6bdedf2747e2
Takumi <13336500-takuoh@users.noreply.gitlab.com>
2024-09-06 21:41:45 +0900

    a

--- console/home.nix
+++ console/home.nix
@@ -103,6 +103,17 @@ in
     enable = true;
     config.theme = "Nord";
   };
+
+  programs.htop = {
+  enable = true;
+  settings = {
+  tree_view = 1;
+  tree_view_always_by_pid = 1;
+  show_cpu_frequency = 1;
+  show_cpu_temperature = 1;
+  };
+  };
+
   nix = {
     package = pkgs.nix;
     settings.experimental-features = [ "nix-command" "flakes" ];
