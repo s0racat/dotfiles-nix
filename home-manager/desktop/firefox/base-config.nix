f602bcf66f14c91f6e24d4019ef4de3a4210fa2b
Takumi <takuoh@tuta.io>
2024-09-29 11:46:54 +0900

    set firefox to default browser

--- home-manager/desktop/firefox/base-config.nix
+++ home-manager/desktop/firefox/base-config.nix
@@ -9,6 +9,21 @@
   #   ./disable-av1.nix
   # ];
   # manage firefox settings
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+    };
+  };
   programs.firefox = {
     enable = true;
     package = null;
