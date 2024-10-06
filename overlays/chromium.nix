3a9392392c176b69245016576181e93a27c128a9
soracat <125882337+s0racat@users.noreply.github.com>
2024-10-06 10:11:53 +0900

    Use vscode.fhs

--- overlays/chromium.nix
+++ overlays/chromium.nix
@@ -18,5 +18,5 @@ let
 in
 {
   chromium = prev.chromium.override { commandLineArgs = chromium_flags; };
-  vscode = prev.vscode.override { commandLineArgs = electron_flags; };
+  vscode.fhs = (prev.vscode.override { commandLineArgs = electron_flags; }).fhs;
 }
