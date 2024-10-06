42449128a29edfcb0c39e0f0bac09ec53dd8fc11
soracat <125882337+s0racat@users.noreply.github.com>
2024-10-06 17:27:40 +0900

    Move user configuration to hosts/um690pro.nix

--- hosts/um690pro.nix
+++ hosts/um690pro.nix
@@ -1,9 +1,13 @@
-{ inputs, lib, ... }:
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ../nixos/hardware/um690pro.nix
     ../nixos/configuration.nix
-    inputs.home-manager.nixosModules.home-manager
     inputs.lanzaboote.nixosModules.lanzaboote
   ];
   networking.hostName = "um690pro";
@@ -13,8 +17,26 @@
     enable = true;
     pkiBundle = "/etc/secureboot";
   };
-  home-manager.users.alice = import ../home-manager/desktop;
-  home-manager.backupFileExtension = "hmbak";
-  # use flake's nixpkgs settings.
-  home-manager.useGlobalPkgs = true;
+  home-manager.users.alice =
+    { ... }:
+    {
+      imports = [
+        ../home-manager/desktop
+      ];
+      home.stateVersion = "24.05"; # Please read the comment before changing.
+    };
+  users.users.alice = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "video"
+      "i2c"
+      "docker"
+      "libvirtd"
+    ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [ ];
+    initialPassword = "123456";
+  };
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -167,20 +167,6 @@
 
   hardware.i2c.enable = true;
   programs.zsh.enable = true;
-  users.users.alice = {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "video"
-      "i2c"
-      "docker"
-      "libvirtd"
-    ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [ ];
-    initialPassword = "123456";
-  };
 
   # sway
   services.gnome.gnome-keyring.enable = true;
