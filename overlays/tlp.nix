e0f0ea20f9d800e9ab15e619b39385a99206a451
soracat <125882337+s0racat@users.noreply.github.com>
2024-10-20 22:10:38 +0900

    fmt

--- overlays/tlp.nix
+++ overlays/tlp.nix
@@ -1,9 +1,8 @@
 # https://github.com/NixOS/nixpkgs/issues/349759
-final: prev:
-{
-tlp = prev.tlp.overrideAttrs (old: {
+final: prev: {
+  tlp = prev.tlp.overrideAttrs (old: {
     makeFlags = (old.makeFlags or [ ]) ++ [
       "TLP_SYSD=/lib/systemd/system"
     ];
-});
+  });
 }
