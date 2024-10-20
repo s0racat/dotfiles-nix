cb5203a8bb94f41fd1896336dd3afa7bb88477cf
soracat <125882337+s0racat@users.noreply.github.com>
2024-10-20 22:10:36 +0900

    docker settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -13,6 +13,10 @@
     ./nix.nix
   ];
   virtualisation.docker.enable = true;
+  virtualisation.docker.daemon.settings = {
+    "ip" = "127.0.0.1";
+    "bip" = "172.18.0.1/24";
+  };
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 
