6000abdbad4eb30759be03c76c45589e11af1f41
soracat <125882337+s0racat@users.noreply.github.com>
2024-10-24 22:42:57 +0900

    Remove tlp overlay

--- overlays/tlp.nix
+++
@@ -1,8 +0,0 @@
-# https://github.com/NixOS/nixpkgs/issues/349759
-final: prev: {
-  tlp = prev.tlp.overrideAttrs (old: {
-    makeFlags = (old.makeFlags or [ ]) ++ [
-      "TLP_SYSD=/lib/systemd/system"
-    ];
-  });
-}
