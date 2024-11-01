138387093004fbdbbf2dd30d579bb16661f58648
soracat <125882337+s0racat@users.noreply.github.com>
2024-11-01 20:47:13 +0900

    Remove sway/packages.nix

--- home-manager/desktop/sway/sway.nix
+++ home-manager/desktop/sway/sway.nix
@@ -271,4 +271,14 @@
     	exec sway
     fi
   '';
+
+  home.packages =
+    let
+      playerctl-notify = pkgs.callPackage ../../../pkgs/playerctl-notify { };
+    in
+    [
+      pkgs.font-awesome_4
+      pkgs.i3status-rust
+      playerctl-notify
+    ];
 }
