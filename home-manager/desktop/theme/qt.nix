eca7eef39e27f147781eb79944f02f158133092b
soracat <125882337+s0racat@users.noreply.github.com>
2024-11-02 12:30:11 +0900

    Remove kvantum

--- home-manager/desktop/theme/qt.nix
+++ home-manager/desktop/theme/qt.nix
@@ -1,24 +1,19 @@
 {
-  pkgs,
   config,
   lib,
   ...
 }:
 let
-  style = "kvantum";
+  style = "Fusion";
+  color_scheme_path = "${config.xdg.configHome}/qt5ct/colors/base16-nord.conf";
+  custom_palette = true;
 in
-rec {
+{
   qt = {
     enable = true;
     platformTheme.name = "qtct";
   };
   xdg.configFile = {
-    "Kvantum/Nordic-bluish-solid".source = "${pkgs.nordic}/share/Kvantum/Nordic-bluish-solid";
-    "Kvantum/kvantum.kvconfig".text = lib.generators.toINI { } {
-      General = {
-        theme = "Nordic-bluish-solid";
-      };
-    };
     "qt5ct/qt5ct.conf".text =
       with config.gtk;
       lib.generators.toINI { } {
@@ -28,7 +23,7 @@ rec {
         };
         Appearance = {
           icon_theme = "${iconTheme.name}";
-          inherit style;
+          inherit style color_scheme_path custom_palette;
         };
       };
     "qt6ct/qt6ct.conf".text =
@@ -40,12 +35,21 @@ rec {
         };
         Appearance = {
           icon_theme = "${iconTheme.name}";
-          inherit style;
+          inherit style color_scheme_path custom_palette;
         };
       };
+    "qt5ct/colors/base16-nord.conf".text = ''
+      /*
+      *
+      * Base16 Nord
+      * Author: arcticicestudio
+      *
+      */
+
+      [ColorScheme]
+      active_colors=#ff88c0d0, #ff3b4252, #ff3b4252, #ffe5e9f0, #ff4c566a, #ffd8dee9, #ffb48ead, #ffeceff4, #ffe5e9f0, #ff3b4252, #ff2e3440, #ff4c566a, #ff434c5e, #ffb48ead, #ffd08770, #ffbf616a, #ff434c5e, #ffe5e9f0, #ff3b4252, #ffb48ead, #8fb48ead
+      disabled_colors=#ff5e81ac, #ff3b4252, #ff3b4252, #ffe5e9f0, #ff4c566a, #ffd8dee9, #ff5e81ac, #ff5e81ac, #ff5e81ac, #ff3b4252, #ff2e3440, #ff4c566a, #ff434c5e, #ffb48ead, #ffd08770, #ffbf616a, #ff434c5e, #ffe5e9f0, #ff3b4252, #ff5e81ac, #8f5e81ac
+      inactive_colors=#ff88c0d0, #ff3b4252, #ff3b4252, #ffe5e9f0, #ff4c566a, #ffd8dee9, #ffb48ead, #ffeceff4, #ffe5e9f0, #ff3b4252, #ff2e3440, #ff4c566a, #ff434c5e, #ffb48ead, #ffd08770, #ffbf616a, #ff434c5e, #ffe5e9f0, #ff3b4252, #ffb48ead, #8fb48ead
+    '';
   };
-  home.packages = with pkgs; [
-    libsForQt5.qtstyleplugin-kvantum
-    qt6Packages.qtstyleplugin-kvantum
-  ];
 }
