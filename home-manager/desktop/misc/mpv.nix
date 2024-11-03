97de6cd13dcaa82cef9af26cc1fc203d8b7329ec
soracat <125882337+s0racat@users.noreply.github.com>
2024-11-03 11:45:03 +0900

    Revert "Update mpv"
    This reverts commit 33a3a5ec359420f0444ea0aae954e9de5908aebb.

--- home-manager/desktop/misc/mpv.nix
+++ home-manager/desktop/misc/mpv.nix
@@ -3,18 +3,18 @@
   lib,
   ...
 }:
-let
-  isNixOS = (import ../sway/isNixOS.nix).isNixOS;
-  scripts = [ pkgs.mpvScripts.mpris ];
-  mpvPackage = if scripts == [ ] then pkgs.mpv else (pkgs.mpv.override { scripts = scripts; });
-in
 {
-  xdg.configFile."mpv/mpv.conf".text = lib.generators.toKeyValue { } {
-    keep-open = "yes";
-    hwdec = "auto";
-    ytdl-format = "bestvideo[height<=720][vcodec!=av1]+bestaudio/best";
-    gpu-hwdec-interop = "vaapi";
-    ao = "pipewire";
+  programs.mpv = rec {
+    enable = true;
+    package = lib.mkIf (builtins.pathExists "/usr/bin/mpv") pkgs.emptyDirectory;
+    scripts = lib.mkIf (package == pkgs.mpv) [ pkgs.mpvScripts.mpris ];
+    config = {
+      keep-open = "yes";
+      hwdec = "auto";
+      ytdl-format = "bestvideo[height<=720][vcodec!=av1]+bestaudio/best";
+      gpu-hwdec-interop = "vaapi";
+      ao = "pipewire";
+    };
   };
-  home.packages = [ pkgs.yt-dlp ] ++ lib.optional isNixOS mpvPackage;
+  home.packages = [ pkgs.yt-dlp ];
 }
