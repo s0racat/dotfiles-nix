fcd0552a1596da6c9dc9666a218a5b230c248024
soracat <125882337+s0racat@users.noreply.github.com>
2024-11-04 22:47:52 +0900

    Update qt.nix

--- home-manager/desktop/theme/qt.nix
+++ home-manager/desktop/theme/qt.nix
@@ -1,12 +1,29 @@
 {
+  pkgs,
   config,
   lib,
   ...
 }:
 let
   style = "Fusion";
-  color_scheme_path = "${config.xdg.configHome}/qt5ct/colors/base16-nord.conf";
+  color_scheme_path = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/indicozy/wmtm/fafcd8a3e3eb09858a365d036e5912627e15b229/configs/Green/config/qt5ct/colors/Nord.conf";
+    hash = "sha256-unWtU48YAuonuCDnegvVvKyEvh7Lu5AtgzUaFKYK53c=";
+  };
   custom_palette = true;
+  qtctConf =
+    with config.gtk;
+    lib.generators.toINI { } {
+      Fonts = {
+        fixed = ''"${font.name},${toString font.size},-1,5,50,0,0,0,0,0,Regular"'';
+        general = ''"${font.name},${toString font.size},-1,5,50,0,0,0,0,0,Regular"'';
+      };
+      Appearance = {
+        icon_theme = "${iconTheme.name}";
+        inherit style color_scheme_path custom_palette;
+      };
+    };
+
 in
 {
   qt = {
@@ -14,42 +31,7 @@ in
     platformTheme.name = "qtct";
   };
   xdg.configFile = {
-    "qt5ct/qt5ct.conf".text =
-      with config.gtk;
-      lib.generators.toINI { } {
-        Fonts = {
-          fixed = ''"${font.name},${toString font.size},-1,5,50,0,0,0,0,0,Regular"'';
-          general = ''"${font.name},${toString font.size},-1,5,50,0,0,0,0,0,Regular"'';
-        };
-        Appearance = {
-          icon_theme = "${iconTheme.name}";
-          inherit style color_scheme_path custom_palette;
-        };
-      };
-    "qt6ct/qt6ct.conf".text =
-      with config.gtk;
-      lib.generators.toINI { } {
-        Fonts = {
-          fixed = ''"${font.name},${toString font.size},-1,5,50,0,0,0,0,0,Regular"'';
-          general = ''"${font.name},${toString font.size},-1,5,50,0,0,0,0,0,Regular"'';
-        };
-        Appearance = {
-          icon_theme = "${iconTheme.name}";
-          inherit style color_scheme_path custom_palette;
-        };
-      };
-    "qt5ct/colors/base16-nord.conf".text = ''
-      /*
-      *
-      * Base16 Nord
-      * Author: arcticicestudio
-      *
-      */
-
-      [ColorScheme]
-      active_colors=#ff88c0d0, #ff3b4252, #ff3b4252, #ffe5e9f0, #ff4c566a, #ffd8dee9, #ffb48ead, #ffeceff4, #ffe5e9f0, #ff3b4252, #ff2e3440, #ff4c566a, #ff434c5e, #ffb48ead, #ffd08770, #ffbf616a, #ff434c5e, #ffe5e9f0, #ff3b4252, #ffb48ead, #8fb48ead
-      disabled_colors=#ff5e81ac, #ff3b4252, #ff3b4252, #ffe5e9f0, #ff4c566a, #ffd8dee9, #ff5e81ac, #ff5e81ac, #ff5e81ac, #ff3b4252, #ff2e3440, #ff4c566a, #ff434c5e, #ffb48ead, #ffd08770, #ffbf616a, #ff434c5e, #ffe5e9f0, #ff3b4252, #ff5e81ac, #8f5e81ac
-      inactive_colors=#ff88c0d0, #ff3b4252, #ff3b4252, #ffe5e9f0, #ff4c566a, #ffd8dee9, #ffb48ead, #ffeceff4, #ffe5e9f0, #ff3b4252, #ff2e3440, #ff4c566a, #ff434c5e, #ffb48ead, #ffd08770, #ffbf616a, #ff434c5e, #ffe5e9f0, #ff3b4252, #ffb48ead, #8fb48ead
-    '';
+    "qt5ct/qt5ct.conf".text = qtctConf;
+    "qt6ct/qt6ct.conf".text = qtctConf;
   };
 }
