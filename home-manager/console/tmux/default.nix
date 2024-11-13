f99dafca3abe23dd70dd38aea8ef0893e0a6f02e
soracat <125882337+s0racat@users.noreply.github.com>
2024-11-13 10:08:31 +0900

    nord.tmuxでbashをつかう

--- home-manager/console/tmux/default.nix
+++ home-manager/console/tmux/default.nix
@@ -14,6 +14,15 @@
     terminal = "tmux-256color";
     historyLimit = 999999999;
     extraConfig = builtins.readFile ./tmux.conf;
-    plugins = with pkgs.tmuxPlugins; [ nord ];
+    plugins = with pkgs.tmuxPlugins; [
+      (nord.overrideAttrs (oldAttrs: {
+        postInstall =
+          (oldAttrs.postInstall or "")
+          + ''
+            sed -i '1i#!/bin/bash' $out/share/tmux-plugins/nord/nord.tmux
+            patchShebangs .
+          '';
+      }))
+    ];
   };
 }
