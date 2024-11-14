57606ef37b97a4827015562b06515c9a8d4ad615
soracat <125882337+s0racat@users.noreply.github.com>
2024-11-14 21:22:25 +0900

    tmux: sensibleOnTop = false

--- home-manager/console/tmux/default.nix
+++ home-manager/console/tmux/default.nix
@@ -13,16 +13,10 @@
     prefix = "c-s";
     terminal = "tmux-256color";
     historyLimit = 999999999;
+    sensibleOnTop = false;
     extraConfig = builtins.readFile ./tmux.conf;
     plugins = with pkgs.tmuxPlugins; [
-      (nord.overrideAttrs (oldAttrs: {
-        postInstall =
-          (oldAttrs.postInstall or "")
-          + ''
-            sed -i '1i#!/bin/bash' $out/share/tmux-plugins/nord/nord.tmux
-            patchShebangs .
-          '';
-      }))
+      nord
     ];
   };
 }
