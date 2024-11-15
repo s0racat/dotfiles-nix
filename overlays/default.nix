7e443e108801dbf5bbd185adaf3fe34774c74240
soracat <125882337+s0racat@users.noreply.github.com>
2024-11-15 13:08:57 +0900

    Remove libskk overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -17,17 +17,6 @@ in
   chromium = prev.chromium.override { commandLineArgs = chromium_flags; };
   vscode.fhs = (prev.vscode.override { commandLineArgs = electron_flags; }).fhs;
   # https://github.com/NixOS/nixpkgs/pull/354218
-  libskk = prev.libskk.overrideAttrs {
-    patches = [
-      # fix parse error in default.json
-      # https://github.com/ueno/libskk/pull/90
-      (prev.fetchpatch {
-        url = "https://github.com/ueno/libskk/commit/2382ebedc8dca88e745d223ad7badb8b73bbb0de.diff";
-        sha256 = "sha256-e1bKVteNjqmr40XI82Qar63LXPWYIfnUVlo5zQSkPNw=";
-      })
-    ];
-
-  };
   tmuxPlugins.nord = prev.tmuxPlugins.nord.overrideAttrs (oldAttrs: {
     postInstall =
       (oldAttrs.postInstall or "")
