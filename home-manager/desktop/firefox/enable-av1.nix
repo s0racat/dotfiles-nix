f8ee82133376ae2331230940a21ebf0e4a1fe1c4
soracat <125882337+s0racat@users.noreply.github.com>
2024-11-15 18:46:08 +0900

    きれいに

--- home-manager/desktop/firefox/enable-av1.nix
+++ home-manager/desktop/firefox/enable-av1.nix
@@ -2,16 +2,7 @@
   ...
 }:
 {
-  programs.firefox = {
-    profiles = {
-      default = {
-        settings =
-          {
-          }
-          // {
-            "media.av1.enabled" = true;
-          };
-      };
-    };
+  programs.firefox.profiles.default.settings = { } // {
+    "media.av1.enabled" = true;
   };
 }
