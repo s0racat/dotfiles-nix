a75da9f97bf770edd4b7db127479feb23c4b2450
soracat <125882337+s0racat@users.noreply.github.com>
2024-11-19 23:24:32 +0900

    change wallpaper

--- home-manager/desktop/sway/sway.nix
+++ home-manager/desktop/sway/sway.nix
@@ -83,24 +83,24 @@ in
           terminal = "foot";
           output =
             let
-              bg =
-                toString (
-                  pkgs.fetchurl {
-                    url = "https://i.pximg.net/img-master/img/2024/09/13/19/05/54/122395805_p0_master1200.jpg";
-                    sha256 = "sha256-Zg9kY6oCbtzUfFIUne9Xy2JWeUk9Q4gyoSVRPcS98u8=";
-                    curlOptsList = [
-                      "-H"
-                      "User-Agent: Mozilla/5.0"
-                      "-H"
-                      "Referer: https://pixiv.net"
-                    ];
-                  }
-                )
-                + " fill";
+              wallpaper = toString (
+                pkgs.fetchurl {
+                  # url = "https://i.pximg.net/img-master/img/2024/09/13/19/05/54/122395805_p0_master1200.jpg";
+                  url = "https://webusstatic.yo-star.com/bluearchive_jp_web/fankit/162687961120015004/01.jpg";
+                  # sha256 = "sha256-Zg9kY6oCbtzUfFIUne9Xy2JWeUk9Q4gyoSVRPcS98u8=";
+                  sha256 = "sha256-5Dbkanay+KP8BXou9NE5jXSzu2itqch8hnNrE7eWQzA=";
+                  curlOptsList = [
+                    "-H"
+                    "User-Agent: Mozilla/5.0"
+                    "-H"
+                    "Referer: https://pixiv.net"
+                  ];
+                }
+              );
             in
             {
               "HDMI-A-1" = {
-                inherit bg;
+                bg = "${wallpaper} fill";
               };
             };
 
