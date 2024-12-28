a8ec99a0f0d90f4d84ee8298d13aaa32c49ba44a
soracat <125882337+s0racat@users.noreply.github.com>
2024-12-28 23:38:19 +0900

    Upd

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -15,7 +15,10 @@
     # fix tofu in systemd boot log
     "locale.LANG=en_US.UTF-8"
   ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # https://www.suse.com/ja-jp/support/kb/doc/?id=000017857
   boot.kernel.sysctl = {
     "vm.dirty_bytes" = 600 * 1024 * 1024;
     "vm.dirty_background_bytes" = 300 * 1024 * 1024;
