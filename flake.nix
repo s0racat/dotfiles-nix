76be6f306b11a32795b31e3cf3533538814226c1
Takumi <125882337+s0racat@users.noreply.github.com>
2024-12-29 10:19:02 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "dotfiles";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
