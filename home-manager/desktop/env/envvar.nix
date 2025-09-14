44f78b06b886fb7a46845db1a70f567422ef1437
soracat <125882337+s0racat@users.noreply.github.com>
2025-09-14 17:36:04 +0900

    upd

--- home-manager/desktop/env/envvar.nix
+++ home-manager/desktop/env/envvar.nix
@@ -1,9 +1,10 @@
-_:
+{ config, ... }:
 let
   envvar = {
 
     MOZ_DBUS_REMOTE = 1;
     MOZ_LEGACY_PROFILES = 1; # fix Profile Missing
+    PATH = "${config.home.profileDirectory}/bin:$PATH";
   };
 in
 {
