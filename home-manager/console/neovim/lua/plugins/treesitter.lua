928f1bb958c2f8ecae3668e93ce45a9aadb02dec
soracat <125882337+s0racat@users.noreply.github.com>
2025-09-28 11:54:31 +0900

    upd

--- home-manager/console/neovim/lua/plugins/treesitter.lua
+++ home-manager/console/neovim/lua/plugins/treesitter.lua
@@ -1,13 +1,21 @@
 local spec = {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            opts.ensure_installed = {}
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {}, -- 必要な言語を指定
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+			})
+		end,
+	},
 
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 }
 return spec
