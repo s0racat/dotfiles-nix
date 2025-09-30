ba5551e77401308e3d42d9fec2a8a35a4cc5b262
soracat <125882337+s0racat@users.noreply.github.com>
2025-09-30 20:46:41 +0900

    upd

--- home-manager/console-wsl/wsl.nix
+++ home-manager/console-wsl/wsl.nix
@@ -5,41 +5,42 @@
   ...
 }:
 let
-  winexeBin =
-    path:
-    toString (pkgs.writeShellScriptBin (builtins.baseNameOf path) ''${lib.escapeShellArg path} $@'');
   winexe =
     path:
-    toString (
-      pkgs.writeShellScript (builtins.replaceStrings [ "." ] [ "_" ] (
-        builtins.baseNameOf path
-      )) ''${lib.escapeShellArg path} $@''
-    );
+    toString (pkgs.writeShellScriptBin (builtins.baseNameOf path) ''${lib.escapeShellArg path} $@'');
+  winExes = paths: builtins.map winexe paths;
 in
 {
   # wslpath 'C:\...'
   # $ dpkg-reconfigure locales
   # select en_US.UTF-8
   home.packages = [
-    (winexeBin "/mnt/c/Windows/explorer.exe")
-    (winexeBin "/mnt/c/Windows/System32/rundll32.exe")
-
+    (pkgs.writeShellScriptBin "clip.exe" ''${pkgs.iconv}/bin/iconv -t sjis | ${lib.escapeShellArg "/mnt/c/Windows/System32/clip.exe"} $@'')
+  ]
+  ++ winExes [
+    "/mnt/c/Windows/explorer.exe"
+    "/mnt/c/Windows/System32/rundll32.exe"
+    "/mnt/c/Program Files/GitHub CLI/gh.exe"
+    "/mnt/c/Program Files/OpenSSH/ssh.exe"
+    "/mnt/c/Program Files/OpenSSH/ssh-add.exe"
+    "/mnt/c/Users/${config.home.username}/AppData/Local/Programs/Microsoft VS Code/bin/code"
+    "/mnt/c/Program Files/Mozilla Firefox/firefox.exe"
   ];
   home.sessionVariables = {
-    GH_TOKEN = "$(${winexe "/mnt/c/Program Files/GitHub CLI/gh.exe"} auth token)";
+    GH_TOKEN = "$(gh.exe auth token)";
     WIN_HOME = "/mnt/c/Users/${config.home.username}";
   };
   programs.git = {
     extraConfig = {
-      core.sshcommand = winexe "/mnt/c/Program Files/OpenSSH/ssh.exe";
+      core.sshcommand = "ssh.exe";
     };
   };
   programs.zsh = {
     shellAliases = {
-      ssh = winexe "/mnt/c/Program Files/OpenSSH/ssh.exe";
-      ssh-add = winexe "/mnt/c/Program Files/OpenSSH/ssh-add.exe";
-      code = winexe "/mnt/c/Users/${config.home.username}/AppData/Local/Programs/Microsoft VS Code/bin/code";
-      firefox = winexe "/mnt/c/Program Files/Mozilla Firefox/firefox.exe";
+      ssh = "ssh.exe";
+      ssh-add = "ssh-add.exe";
+      code = "code";
+      firefox = "firefox.exe";
     };
     initContent = ''
       wwhich() {
