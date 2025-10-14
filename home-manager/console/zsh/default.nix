7a5764158c1eca5d5b2b2373239831b17b4d3f73
soracat <125882337+s0racat@users.noreply.github.com>
2025-10-14 23:00:01 +0900

    upd

--- home-manager/console/zsh/default.nix
+++ home-manager/console/zsh/default.nix
@@ -5,7 +5,18 @@
   isNixOS,
   ...
 }:
+
+let
+  dircolors = builtins.readFile (
+    pkgs.runCommandNoCC "dircolors" { }
+      "${pkgs.coreutils}/bin/dircolors -b ${config.home.file.".dir_colors".source} > $out"
+  );
+  zoxide = pkgs.runCommandNoCC "zoxide" { } "${pkgs.zoxide}/bin/zoxide init zsh > $out";
+  starship = pkgs.runCommandNoCC "starship" { } "${pkgs.starship}/bin/starship init zsh > $out";
+  fzf = pkgs.runCommandNoCC "fzf" { } "${pkgs.fzf}/bin/fzf --zsh > $out";
+in
 {
+
   programs.dircolors = {
     enable = true;
     enableZshIntegration = false;
@@ -31,7 +42,7 @@
       gksu = "pkexec env WAYLAND_DISPLAY=$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY XDG_RUNTIME_DIR=/run/user/0 DISPLAY=$DISPLAY";
       npm = "pnpm";
       cz = "git cz";
-      nix-shell = "nix-shell --command $(command -v zsh)";
+      nix-shell = "nix-shell --command /bin/zsh";
       rm = "rip";
       lg = "lazygit";
       ll = "eza -F -oalg --time-style=long-iso";
@@ -61,28 +72,67 @@
       oct = "stat -c '%A %a %n'";
     };
     # zprof.enable = true;
-    initContent =
-      let
-        dircolors = builtins.readFile (
-          pkgs.runCommandNoCCLocal "dircolors" { }
-            "${pkgs.coreutils}/bin/dircolors -b ${config.home.file.".dir_colors".source} > $out"
-        );
-        zoxide = pkgs.runCommandNoCCLocal "zoxide" { } "${pkgs.zoxide}/bin/zoxide init zsh > $out";
-        starship = pkgs.runCommandNoCCLocal "starship" { } "${pkgs.starship}/bin/starship init zsh > $out";
-      in
-      ''
-        source ${pkgs.zsh-defer}/share/zsh-defer/zsh-defer.plugin.zsh
-        source ${starship}
-        zsh-defer source ${pkgs.fzf}/share/fzf/key-bindings.zsh
-        ${dircolors}
-        zsh-defer source ${zoxide}
-        autoload -U compinit &&  zsh-defer compinit
-        source ${config.programs.nix-index.package}/etc/profile.d/command-not-found.sh
-        zsh-defer source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-        zsh-defer source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-      ''
-      + builtins.readFile ./zshrc;
+    initContent = ''
+      function source {
+        ensure_zcompiled $1
+        builtin source $1
+      }
+      function ensure_zcompiled {
+        local compiled="$1.zwc"
+        if [[ ! -r "$compiled" || "$1" -nt "$compiled" ]]; then
+          echo "\033[1;36mCompiling\033[m $1"
+          zcompile $1
+        fi
+      }
+      ensure_zcompiled ~/.zshrc
+      source ${config.xdg.cacheHome}/sheldon.zsh
+      ${dircolors}
+    ''
+    + builtins.readFile ./zshrc;
 
+  };
+
+  home.packages = [ pkgs.sheldon ];
+  xdg.configFile."sheldon/sync/starship.zsh".source = starship;
+  xdg.configFile."sheldon/async/fzf.zsh".source = fzf;
+  xdg.configFile."sheldon/async/zoxide.zsh".source = zoxide;
+  xdg.configFile."sheldon/async/command-not-found.zsh".source =
+    "${config.programs.nix-index.package}/etc/profile.d/command-not-found.sh";
+  xdg.configFile."sheldon/plugins.toml" = {
+    onChange = "${pkgs.sheldon}/bin/sheldon source --update 2>/dev/null > ${config.xdg.cacheHome}/sheldon.zsh";
+    text = ''
+      shell = "zsh"
+
+      [templates]
+      defer = """{% for file in files %}zsh-defer source "{{ file }}"\n{% endfor %}"""
+
+      [plugins.zsh-defer]
+      github = "romkatv/zsh-defer"
+
+      [plugins.starship]
+      inline = "source ~/.config/sheldon/sync/starship.zsh"
+
+      [plugins.zoxide]
+      inline = "zsh-defer source ~/.config/sheldon/async/zoxide.zsh"
+
+      [plugins.compinit]
+      inline = "autoload -U compinit && zsh-defer compinit"
+
+
+      [plugins.fzf]
+      inline = "zsh-defer source ~/.config/sheldon/async/fzf.zsh"
+
+      [plugins.command-not-found]
+      inline = "zsh-defer source ~/.config/sheldon/async/command-not-found.zsh"
+
+      [plugins.zsh-autosuggestions]
+      apply = ["defer"]
+      github = "zsh-users/zsh-autosuggestions"
+
+      [plugins.zsh-syntax-highlighting]
+      apply = ["defer"]
+      github = "zsh-users/zsh-syntax-highlighting"
+    '';
   };
 
 }
