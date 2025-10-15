d8dcd6cc12e6fc286577cc50b59862e3f1f023eb
soracat <125882337+s0racat@users.noreply.github.com>
2025-10-15 14:34:38 +0900

    upd

--- home-manager/console/zsh/default.nix
+++ home-manager/console/zsh/default.nix
@@ -14,6 +14,9 @@ let
   zoxide = pkgs.runCommandNoCC "zoxide" { } "${pkgs.zoxide}/bin/zoxide init zsh > $out";
   starship = pkgs.runCommandNoCC "starship" { } "${pkgs.starship}/bin/starship init zsh > $out";
   fzf = pkgs.runCommandNoCC "fzf" { } "${pkgs.fzf}/bin/fzf --zsh > $out";
+  cfg = config.programs.zsh;
+  relToDotDir = file: (lib.optionalString (cfg.dotDir != null) (cfg.dotDir + "/")) + file;
+  zshBin = if isNixOS then "${pkgs.zsh}/bin/zsh" else "/bin/zsh";
 in
 {
 
@@ -42,7 +45,7 @@ in
       gksu = "pkexec env WAYLAND_DISPLAY=$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY XDG_RUNTIME_DIR=/run/user/0 DISPLAY=$DISPLAY";
       npm = "pnpm";
       cz = "git cz";
-      nix-shell = "nix-shell --command /bin/zsh";
+      nix-shell = "nix-shell --command ${zshBin}";
       rm = "rip";
       lg = "lazygit";
       ll = "eza -F -oalg --time-style=long-iso";
@@ -71,37 +74,67 @@ in
       gl = "git pull";
       oct = "stat -c '%A %a %n'";
     };
-    zprof.enable = true;
+    # zprof.enable = true;
+    setOptions = [
+      "correct"
+      "interactivecomments"
+      "no_beep"
+    ];
     initContent = ''
-      function source {
-        ensure_zcompiled $1
-        builtin source $1
-      }
-      function ensure_zcompiled {
-        local compiled="$1.zwc"
-        if [[ ! -r "$compiled" || "$1" -nt "$compiled" ]]; then
-          echo "\033[1;36mCompiling\033[m $1"
-          zcompile $1
-        fi
-      }
-      # ensure_zcompiled ~/.zshrc
-      # ensure_zcompiled ~/.zshenv
       source ${config.xdg.cacheHome}/sheldon.zsh
       ${dircolors}
-      zsh-defer unfunction source
     ''
     + builtins.readFile ./zshrc;
 
   };
 
+  home.file."${relToDotDir ".zshrc"}" = {
+    onChange = ''
+      ${zshBin} -c '[ "${relToDotDir ".zshrc"}" -nt "${relToDotDir ".zshrc"}.zwc" ] && zcompile "${relToDotDir ".zshrc"}"'
+    '';
+  };
+
+  home.file."${relToDotDir ".zshenv"}" = {
+    onChange = ''
+      ${zshBin} -c '[ "${relToDotDir ".zshenv"}" -nt "${relToDotDir ".zshenv"}.zwc" ] && zcompile "${relToDotDir ".zshenv"}"'
+    '';
+  };
+
   home.packages = [ pkgs.sheldon ];
-  xdg.configFile."sheldon/sync/starship.zsh".source = starship;
-  xdg.configFile."sheldon/async/fzf.zsh".source = fzf;
-  xdg.configFile."sheldon/async/zoxide.zsh".source = zoxide;
-  xdg.configFile."sheldon/async/command-not-found.zsh".source =
-    "${config.programs.nix-index.package}/etc/profile.d/command-not-found.sh";
+
+  xdg.configFile."sheldon/sync/starship.zsh" = {
+    source = starship;
+    onChange = ''
+      ${zshBin} -c '[ "$HOME/.config/sheldon/sync/starship.zsh" -nt "$HOME/.config/sheldon/sync/starship.zsh.zwc" ] && zcompile "$HOME/.config/sheldon/sync/starship.zsh"'
+    '';
+  };
+
+  xdg.configFile."sheldon/async/fzf.zsh" = {
+    source = fzf;
+    onChange = ''
+      ${zshBin} -c '[ "$HOME/.config/sheldon/async/fzf.zsh" -nt "$HOME/.config/sheldon/async/fzf.zsh.zwc" ] && zcompile "$HOME/.config/sheldon/async/fzf.zsh"'
+    '';
+  };
+
+  xdg.configFile."sheldon/async/zoxide.zsh" = {
+    source = zoxide;
+    onChange = ''
+      ${zshBin} -c '[ "$HOME/.config/sheldon/async/zoxide.zsh" -nt "$HOME/.config/sheldon/async/zoxide.zsh.zwc" ] && zcompile "$HOME/.config/sheldon/async/zoxide.zsh"'
+    '';
+  };
+
+  xdg.configFile."sheldon/async/command-not-found.zsh" = {
+    source = "${config.programs.nix-index.package}/etc/profile.d/command-not-found.sh";
+    onChange = ''
+      ${zshBin} -c '[ "$HOME/.config/sheldon/async/command-not-found.zsh" -nt "$HOME/.config/sheldon/async/command-not-found.zsh.zwc" ] && zcompile "$HOME/.config/sheldon/async/command-not-found.zsh"'
+    '';
+  };
+
   xdg.configFile."sheldon/plugins.toml" = {
-    onChange = "${pkgs.sheldon}/bin/sheldon source --update 2>/dev/null > ${config.xdg.cacheHome}/sheldon.zsh";
+    onChange = ''
+      ${pkgs.sheldon}/bin/sheldon source --update 2>/dev/null > "${config.xdg.cacheHome}/sheldon.zsh"
+      ${zshBin} -c '[ "${config.xdg.cacheHome}/sheldon.zsh" -nt "${config.xdg.cacheHome}/sheldon.zsh.zwc" ] && zcompile -R "${config.xdg.cacheHome}/sheldon.zsh"'
+    '';
     text = ''
       shell = "zsh"
 
@@ -119,7 +152,6 @@ in
 
       [plugins.compinit]
       inline = "autoload -U compinit && zsh-defer compinit"
-
 
       [plugins.fzf]
       inline = "zsh-defer source ~/.config/sheldon/async/fzf.zsh"
