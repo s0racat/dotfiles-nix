9e1bd3e7f509b03dd0b294f9224357638b25f0c8
Takumi <125882337+s0racat@users.noreply.github.com>
2025-12-01 19:40:37 +0900

    flake.nix の更新

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "dotfiles";
   inputs = {
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     home-manager = {
       url = "github:nix-community/home-manager/master";
