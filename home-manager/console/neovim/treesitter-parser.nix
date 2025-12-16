d4c3514c0ba0710d45a8bac76239e99c04f52377
soracat <125882337+s0racat@users.noreply.github.com>
2025-12-16 18:05:47 +0900

    upd

--- home-manager/console/neovim/treesitter-parser.nix
+++ home-manager/console/neovim/treesitter-parser.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 {
   # https://github.com/nvim-treesitter/nvim-treesitter#i-get-query-error-invalid-node-type-at-position
+  # https://zenn.dev/qitoy/articles/nvim-treesitter-main
+  # https://github.com/NixOS/nixpkgs/issues/415438
   xdg.configFile."nvim/parser".source =
     let
       parsers = pkgs.symlinkJoin {
