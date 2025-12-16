6672ddb7ddae1a893296ced65eb882c763cdcc0f
soracat <125882337+s0racat@users.noreply.github.com>
2025-12-16 23:37:05 +0900

    upd

--- home-manager/desktop/cinnamon/dconf.nix
+++ home-manager/desktop/cinnamon/dconf.nix
@@ -1,13 +1,17 @@
+{ config, ... }:
 {
   dconf.settings = {
 
-    "org/cinnamon/desktop/interface" = {
-      font-name = "Roboto 12";
-    };
     "org/cinnamon/desktop/wm/preferences" = {
       titlebar-font = "Roboto Medium 11";
     };
     "org/x/apps/portal".color-scheme = "prefer-dark";
+    "org/cinnamon/desktop/interface" = with config.gtk; {
+      icon-theme = iconTheme.name;
+      gtk-theme = theme.name;
+      cursor-theme = cursorTheme.name;
+      font-name = "Roboto 12";
+    };
   };
 
 }
