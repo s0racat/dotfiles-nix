ab31856f27b5f340ed5f1fa7bd7c0c09e7fa52cb
soracat <125882337+s0racat@users.noreply.github.com>
2025-12-27 14:43:35 +0900

    upd

--- home-manager/console/neovim/lua/plugins/lazygit.lua
+++ home-manager/console/neovim/lua/plugins/lazygit.lua
@@ -1,18 +1,18 @@
 local spec = {
-	"kdheepak/lazygit.nvim",
-	lazy = true,
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
-	},
-	-- optional for floating window border decoration
-	-- setting the keybinding for LazyGit with 'keys' is recommended in
-	-- order to load the plugin when the command is run for the first time
-	keys = {
-		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-	},
+	-- "kdheepak/lazygit.nvim",
+	-- lazy = true,
+	-- cmd = {
+	-- 	"LazyGit",
+	-- 	"LazyGitConfig",
+	-- 	"LazyGitCurrentFile",
+	-- 	"LazyGitFilter",
+	-- 	"LazyGitFilterCurrentFile",
+	-- },
+	-- -- optional for floating window border decoration
+	-- -- setting the keybinding for LazyGit with 'keys' is recommended in
+	-- -- order to load the plugin when the command is run for the first time
+	-- keys = {
+	-- 	{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+	-- },
 }
 return spec
