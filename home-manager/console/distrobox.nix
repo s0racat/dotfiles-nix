bf5acc493d87b36774f4fd9194022cc8c643b0dc
soracat <125882337+s0racat@users.noreply.github.com>
2026-01-20 14:55:22 +0900

    upd

--- home-manager/console/distrobox.nix
+++ home-manager/console/distrobox.nix
@@ -1,9 +1,8 @@
 {
   xdg.configFile."distrobox/distrobox.conf".text = ''
-    container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
     container_user_custom_home="$HOME/distrobox"
     container_manager="podman"
-    container_image_default="docker.io/library/archlinux:latest"
+    container_image_default="quay.io/toolbx/arch-toolbox:latest"
     container_name_default="arch"
   '';
 }
