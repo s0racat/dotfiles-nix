1fc2802a69ae08240c71b95934835e6b05775741
soracat <125882337+s0racat@users.noreply.github.com>
2026-01-20 22:32:28 +0900

    upd

--- home-manager/console/distrobox.nix
+++ home-manager/console/distrobox.nix
@@ -1,16 +1,16 @@
 {
   xdg.configFile."distrobox/distrobox.conf".text = ''
-    container_user_custom_home="$HOME/distrobox"
     container_manager="podman"
     container_image_default="quay.io/toolbx/arch-toolbox:latest"
     container_name_default="arch"
+    container_home_prefix="$HOME/distrobox"
   '';
   systemd.user.services.distrobox-upgrade = {
     Unit.Description = "distrobox-upgrade";
     Service = {
       ExecStart = "distrobox-upgrade --all";
       Type = "simple";
-      StandardOutput = "null";
+      # StandardOutput = "null";
     };
   };
 
