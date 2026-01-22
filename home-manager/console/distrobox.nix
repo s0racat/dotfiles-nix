d88abcad405de00bc0ddb063d1748cc05e38e2d1
soracat <125882337+s0racat@users.noreply.github.com>
2026-01-22 23:41:00 +0900

    upd

--- home-manager/console/distrobox.nix
+++ home-manager/console/distrobox.nix
@@ -5,6 +5,18 @@
     container_name_default="arch"
     container_home_prefix="$HOME/distrobox"
   '';
+  xdg.configFile."distrobox/distrobox.ini".text = ''
+    [arch]
+    image="quay.io/toolbx/arch-toolbox:latest"
+    additional_flags="--env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/host/run/dbus/system_bus_socket"
+    additional_packages="vim pacman-contrib"
+    exported_bins="/usr/bin/makepkg /usr/bin/updpkgsums"
+
+    [kali]
+    image="docker.io/kalilinux/kali-rolling:latest"
+    additional_packages="kali-linux-headless"
+    additional_flags="--env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/host/run/dbus/system_bus_socket"
+  '';
   systemd.user.services.distrobox-upgrade = {
     Unit.Description = "distrobox-upgrade";
     Service = {
