8c3f554768c31c735e171dbe0d1627268bc81fd0
soracat <125882337+s0racat@users.noreply.github.com>
2026-01-28 00:09:44 +0900

    upd

--- home-manager/common/nixPath.nix
+++ home-manager/common/nixPath.nix
@@ -1,13 +1,13 @@
 {
   lib,
   inputs,
-  config,
   ...
 }:
 {
   # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
   nix.registry.nixpkgs.flake = inputs.nixpkgs-unstable;
-  nix.nixPath = [ "nixpkgs=${config.xdg.dataHome}/nix/inputs/nixpkgs-unstable" ];
+  nix.registry.home-manager.flake = inputs.home-manager;
+  nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
   xdg.dataFile = lib.mapAttrs' (name: value: {
     name = "nix/inputs/${name}";
     value = {
