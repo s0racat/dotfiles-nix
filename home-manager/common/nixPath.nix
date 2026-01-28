1636014264a96163efe34504a2ff01dfbe30969a
Takumi <125882337+s0racat@users.noreply.github.com>
2026-01-28 15:24:41 +0900

    nixPath.nix の更新

--- home-manager/common/nixPath.nix
+++ home-manager/common/nixPath.nix
@@ -4,7 +4,7 @@
   ...
 }:
 {
-  # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+  # make `nix run nixpkgs#hello` use the same nixpkgs as the one used by this flake.
   nix.registry.nixpkgs.flake = inputs.nixpkgs-unstable;
   nix.registry.home-manager.flake = inputs.home-manager;
   nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
