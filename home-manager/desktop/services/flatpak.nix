e2b6fee2e84e8cc664593350779ed631abb0e622
soracat <125882337+s0racat@users.noreply.github.com>
2026-01-31 23:43:00 +0900

    upd

--- home-manager/desktop/services/flatpak.nix
+++ home-manager/desktop/services/flatpak.nix
@@ -1,4 +1,36 @@
-{ lib, isNixOS, ... }:
+{
+  lib,
+  isNixOS,
+  pkgs,
+  config,
+  ...
+}:
 
 lib.mkIf (!isNixOS) {
+  xdg.configFile."autostart/mintupdate.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Hidden=true
+  '';
+  home.file.".local/bin/flatpak-update" = {
+    source = pkgs.writeShellScript "flatpak-update" ''
+
+      OUTPUT=$(flatpak update -y --noninteractive 2>&1)
+
+      COUNT=$(echo "$OUTPUT" | grep -c "^Updating ")
+
+      if [ "$COUNT" -gt 0 ]; then
+        echo "$COUNT 個のアプリが更新されました"
+        notify-send "📦 Flatpak Update" "$COUNT 個のアプリが更新されました"
+      fi
+    '';
+    executable = true;
+  };
+  xdg.configFile."autostart/flatpak-update.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Flatpak Update
+    Exec=${config.home.homeDirectory}/.local/bin/flatpak-update
+    X-GNOME-Autostart-enabled=true
+  '';
 }
