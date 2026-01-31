a25045ed65949d0943330381d0b34d943d88c2cb
soracat <125882337+s0racat@users.noreply.github.com>
2026-01-31 22:28:04 +0900

    rm flatpak timer

--- home-manager/desktop/services/flatpak.nix
+++ home-manager/desktop/services/flatpak.nix
@@ -1,20 +1,4 @@
 { lib, isNixOS, ... }:
 
 lib.mkIf (!isNixOS) {
-  systemd.user.services.flatpak-update = {
-    Unit.Description = "flatpak-update";
-    Service = {
-      ExecStart = "/usr/bin/flatpak update --user --assumeyes --noninteractive";
-      Type = "oneshot";
-    };
-  };
-
-  systemd.user.timers.flatpak-update = {
-    Unit.Description = "flatpak-update";
-    Timer = {
-      OnCalendar = "daily";
-      Persistent = true;
-    };
-    Install.WantedBy = [ "timers.target" ];
-  };
 }
