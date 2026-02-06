dd5a274787b0034b74cfba6f9ee61608e6b1faea
soracat <125882337+s0racat@users.noreply.github.com>
2026-02-06 23:32:54 +0900

    a

--- home-manager/console/distrobox.nix
+++ home-manager/console/distrobox.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs,  ... }:
 {
   xdg.configFile."distrobox/distrobox.conf".text = ''
     container_manager="podman"
@@ -6,22 +6,27 @@
     container_name_default="arch"
     container_home_prefix="$HOME/distrobox"
   '';
-  xdg.configFile."distrobox/distrobox.ini".text = ''
-    [arch]
-    image="quay.io/toolbx/arch-toolbox:latest"
-    additional_flags="--env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/host/run/dbus/system_bus_socket"
-    additional_packages="vim pacman-contrib"
-    exported_bins="/usr/bin/makepkg /usr/bin/updpkgsums"
+  xdg.configFile = {
+    "distrobox/arch.ini".text = ''
+      [arch]
+      image="quay.io/toolbx/arch-toolbox:latest"
+      additional_flags="--env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/host/run/dbus/system_bus_socket"
+      additional_packages="vim pacman-contrib"
+      exported_bins="/usr/bin/makepkg /usr/bin/updpkgsums"
+    '';
+    "distrobox/kali.ini".text = ''
+      [kali]
+      image="docker.io/kalilinux/kali-rolling:latest"
+      additional_flags="--env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/host/run/dbus/system_bus_socket"
+      additional_packages="kali-linux-headless"
+    '';
+    "distrobox/blackarch.ini".text = ''
+      [blackarch]
+      image="docker.io/blackarchlinux/blackarch:latest"
+      additional_flags="--env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/host/run/dbus/system_bus_socket"
+    '';
+  };
 
-    #[kali]
-    #image="docker.io/kalilinux/kali-rolling:latest"
-    #additional_flags="--env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/host/run/dbus/system_bus_socket"
-    #additional_packages="kali-linux-headless"
-
-    [blackarch]
-    image="docker.io/blackarchlinux/blackarch:latest"
-    additional_flags="--env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/host/run/dbus/system_bus_socket"
-  '';
   systemd.user.services.distrobox-upgrade = {
     Unit.Description = "distrobox-upgrade";
     Service = {
