bafc0dc00036951dca89b28f4c24e2e3fcc9e65d
soracat <125882337+s0racat@users.noreply.github.com>
2026-02-26 20:45:08 +0900

    upd

--- home-manager/desktop/env/picom.nix
+++ home-manager/desktop/env/picom.nix
@@ -1,3 +1,26 @@
+{ pkgs, ... }:
 {
   xdg.configFile."picom/picom.conf".source = ./picom.conf;
+  xdg.configFile."autostart/picom.desktop".text = ''
+    [Desktop Entry]
+    Version=1.0
+    Type=Application
+    NoDisplay=false
+    Name=picom
+    GenericName=X compositor
+    Comment=An X compositor
+    Categories=Utility;
+    Keywords=compositor;composite manager;window effects;transparency;opacity;
+    TryExec=picom
+    Exec=${pkgs.writeShellScript "picom" ''
+      if [ -n "$XRDP_SESSION" ]; then
+        picom --backend xrender;
+      else
+        picom;
+      fi''}
+    StartupNotify=false
+    Terminal=false
+    # Thanks to quequotion for providing this file!
+    Icon=picom
+  '';
 }
