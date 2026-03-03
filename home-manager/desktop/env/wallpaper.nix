d8c1fd9349cc40ca92dd7445d6519febd371a266
soracat <125882337+s0racat@users.noreply.github.com>
2026-03-03 22:35:25 +0900

    a

--- home-manager/desktop/env/wallpaper.nix
+++ home-manager/desktop/env/wallpaper.nix
@@ -1,4 +1,5 @@
 { self, ... }:
 {
-  home.file."Pictures/2e3440.png".source = "${self}/2e3440.png";
+  home.file."Pictures/2e3440.png".source = "${self}/wallpapar/2e3440.png";
+  home.file."Pictures/3b4252.png".source = "${self}/wallpapar/3b4252.png";
 }
