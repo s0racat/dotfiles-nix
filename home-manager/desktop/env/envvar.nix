02eeda8c8ecd0ed473759d2ccde590c46ea22ce9
soracat <125882337+s0racat@users.noreply.github.com>
2026-03-15 19:56:40 +0900

    a

--- home-manager/desktop/env/envvar.nix
+++ home-manager/desktop/env/envvar.nix
@@ -5,10 +5,15 @@ let
     MOZ_DBUS_REMOTE = 1;
     MOZ_LEGACY_PROFILES = 1; # fix Profile Missing
     TERMINAL = "alacritty";
+
   };
 in
 {
   home.sessionVariables = envvar;
-  systemd.user.sessionVariables = builtins.removeAttrs config.home.sessionVariables [ "NIX_PATH" ];
+  systemd.user.sessionVariables =
+    (builtins.removeAttrs config.home.sessionVariables [ "NIX_PATH" ])
+    // {
+      XCURSOR_PATH = builtins.concatStringsSep ":" config.home.sessionSearchVariables.XCURSOR_PATH;
+    };
 
 }
